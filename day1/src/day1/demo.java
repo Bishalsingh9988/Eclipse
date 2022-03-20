d671f530180485e2d3691313a9afee0f69a04d6c
bishal <bishalsingh9988@gmail.com>
2022-03-20 21:15:22 +0530

    Sacnner class added

--- day1/src/day1/demo.java
+++ day1/src/day1/demo.java
@@ -1,9 +1,12 @@
 package day1;
 
+import java.util.Scanner;
+
 public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
 		System.out.println("hello");
 
 	}
