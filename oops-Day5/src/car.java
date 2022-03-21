a6542f17d836f942e341741f33d77bed31178cea
bishal <bishalsingh9988@gmail.com>
2022-03-21 11:33:24 +0530

    making car class and object.

---
+++ oops-Day5/src/car.java
@@ -0,0 +1,30 @@
+
+public class car {
+
+	
+		int noofcars;
+		String Model;
+		void display()
+		{
+			System.out.println("Welcome to kia-Motors");
+		
+		}
+		void display2()
+		{
+			System.out.println("Welcome to benz");
+		}
+		void display3()
+		{	
+			System.out.println("Welcome to swift");
+		}
+		
+		public static void main(String[] args)
+		{
+			car obj = new car();
+			obj.display();
+			obj.display2();
+			obj.display3();
+		
+	}
+
+}
