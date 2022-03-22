9fb52eff5c1d428f436d6c45b85588b5ed8a55dc
bishal <bishalsingh9988@gmail.com>
2022-03-22 17:13:23 +0530

    using public,int, long,byte, short,import,float keyword in code.

---
+++ OOPS-Day14/src/B.java
@@ -0,0 +1,16 @@
+
+public class B {
+
+	public static void main(String[] args) {
+		int a=12;
+		long b=2029929;
+		byte c=125;
+		short d= 345;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+	}
+
+}
---
+++ OOPS-Day14/src/c.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class c {
+
+	public static void main(String[] args)
+	{	System.out.println("give the input");
+		Scanner sc = new Scanner(System.in);
+		float n = sc.nextFloat();
+		System.out.println(n);
+		
+		
+		
+
+	}
+
+}
